746dc974973b1a9cae689d9e9da14c6bec7ed193
Brenden Judson <brenden@Brendens-MacBook-Air.local>
2022-03-16 12:38:58 -0400

    add env var to build spec

--- infrastructure/deployment_pipelines/ios/deploy.sh
+++ infrastructure/deployment_pipelines/ios/deploy.sh
@@ -1,1 +1,19 @@
-aws cloudformation deploy --stack-name klubby-deployment-ios-dev --template template.yaml --region us-east-1 --capabilities CAPABILITY_IAM --parameter-override GitHubOAuthToken=$1
+aws cloudformation deploy \
+    --stack-name klubby-deployment-ios-prod \
+    --template template.yaml \
+    --region us-east-1 \
+    --capabilities CAPABILITY_IAM \
+    --parameter-override \
+      GitHubOAuthToken=$1 \
+      Stage=prod \
+      GitHubBranch=main
+
+aws cloudformation deploy \
+    --stack-name klubby-deployment-ios-dev \
+    --template template.yaml \
+    --region us-east-1 \
+    --capabilities CAPABILITY_IAM \
+    --parameter-override \
+      GitHubOAuthToken=$1 \
+      Stage=dev \
+      GitHubBranch=dev

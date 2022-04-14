ef2781bba61cdac6e8b210f701e94ac7bc62713b
Brenden Judson <bjudson@trek10.com>
2022-04-14 13:44:14 -0400

    add set wallets test

--- infrastructure/appsync/test/set_wallets.js
+++ infrastructure/appsync/test/set_wallets.js
@@ -8,7 +8,7 @@ var fetch = require('cross-fetch/polyfill');
 const { Token } = require("graphql");
 
 //token needs to be updated to run
-const token = "eyJraWQiOiIwUldMeEprQ041UE5LQmljMmhIaWN5TWhIXC9JVmVDQmFBbmNDbTNCXC96TlU9IiwiYWxnIjoiUlMyNTYifQ.eyJhdF9oYXNoIjoib1BUQTlVZk9yMnkxUDZ0T2c0V3c3ZyIsInN1YiI6IjJjZDQwMDkyLWVhNjYtNDk3ZC1iOTA2LWFmZjMzOTRmZjIzYSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9vRGhvdU5pMkIiLCJjb2duaXRvOnVzZXJuYW1lIjoiYXNkZiIsImF1ZCI6IjNwbmk1OTdodGhuZWJxMjFic3ZwdmkydHFpIiwiZXZlbnRfaWQiOiI4YTEzNzI0Yy1mZGNiLTQwZDctYWM0OS1kMDM2NDBlYTNlMmMiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTY0OTk1NjYxNCwiZXhwIjoxNjQ5OTYwMjE0LCJpYXQiOjE2NDk5NTY2MTQsImp0aSI6Ijc3NjNmOGM5LTI3M2YtNDdkZi04MDU4LWUzMmQ1YTUwMzhhNiIsImVtYWlsIjoiYnJlbmRlbi5qdWRzb25AZ21haWwuY29tIn0.l1Ow_CMfaxcoaPo8s6zBfev2bZ_VSi8oBoSrJhC43RMP6nOUMhG4AdcKfC5DDRn3U6HfeE76jSN3ZbTCOYul1z5XwXzKvuRFihRGKdyoP_7KzdXVdW6FaPXutU6qcbMMknyXJ-Q6rDfKHGj-zPoD5-2Ei83n3gXNnMblnkbvcbpGY4jwJtRyt8QyMBCsp2FAfqijEc2Mt6dMAam5EOMHAa1_p_3023c7u90nYc3zZqnQZuJEwNf9g82Ipids3ZvUQMmPVlOkKHAbp9tkWacIFvPGiT1Ce_BkNjkhtUuddsBEshLC6JE4ImipNAgyibphhQHmo5sbt-6hwFbgsniyew"
+const token = ""
 username = 'asdf'
 const URL = "https://tntqao5pj5eblmhvww7nd4byne.appsync-api.us-east-1.amazonaws.com/graphql"
 
@@ -42,7 +42,7 @@ client
     mutation: gql`
         mutation SetWallet {
             updateUser(username: "${username}", wallets: "testtest") {
-            wallets
+                wallets
             }
         }
     `

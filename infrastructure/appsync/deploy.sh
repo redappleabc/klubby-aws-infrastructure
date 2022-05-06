5cbc23184c6f0294abdc9c02849d14fca49cc3b4
redapple961129 <redapple961129@hotmail.com>
2022-05-06 10:56:43 +0900

    delete resolver

--- infrastructure/appsync/deploy.sh
+++ infrastructure/appsync/deploy.sh
@@ -1,5 +1,11 @@
+delete-resolver
+    --api-id klubby-graphql-dev
+    --type-name Query
+    --field-name getUsers getUserWallets
+
 aws cloudformation deploy \
     --stack-name klubby-appsync-dev \
     --template template.yaml \
     --region us-east-1 \
     --capabilities CAPABILITY_IAM
+    

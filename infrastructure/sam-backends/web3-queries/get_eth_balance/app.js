31e1645bdda9062de5d6ef440f8cb233133ec2bf
Brenden Judson <bjudson@trek10.com>
2022-06-07 15:37:39 -0400

    add first erc20 tokens

--- infrastructure/sam-backends/web3-queries/get_eth_balance/app.js
+++ infrastructure/sam-backends/web3-queries/get_eth_balance/app.js
@@ -6,9 +6,51 @@ const Web3 = require('web3')
 const TABLE_NAME = "klubby-storage-dynamodb-dev-UserTable-IM4YXWAHTLAF"
 const RPC_URL="http://18.206.231.219:8545"
 
+KINSHU_ADDRESS = "0xA2b4C0Af19cC16a6CfAcCe81F192B024d625817D"
+SANSHU_ADDRESS = "0xc73c167e7a4ba109e4052f70d5466d0c312a344d"
+
+// The minimum ABI to get ERC20 Token balance
+const minABI = [
+    // balanceOf
+    {
+      "constant":true,
+      "inputs":[{"name":"_owner","type":"address"}],
+      "name":"balanceOf",
+      "outputs":[{"name":"balance","type":"uint256"}],
+      "type":"function"
+    },
+    // decimals
+    {
+      "constant":true,
+      "inputs":[],
+      "name":"decimals",
+      "outputs":[{"name":"","type":"uint8"}],
+      "type":"function"
+    }
+  ];
+  
+
 //funuction to get eth balance
-async function getBalance(web3,walletAddress){
+async function getEthBalance(web3,walletAddress){
     const balance = await web3.eth.getBalance(walletAddress);
+
+    return balance
+}
+
+//funuction to get kishu inu balance
+async function getKishuBalance(web3,walletAddress){
+    const contract = new web3.eth.Contract(minABI,KINSHU_ADDRESS);
+
+    const balance = await contract.methods.balanceOf(walletAddress).call();
+
+    return balance
+}
+
+//funuction to get sanshu inu balance
+async function getSanshuBalance(web3,walletAddress){
+    const contract = new web3.eth.Contract(minABI,SANSHU_ADDRESS);
+
+    const balance = await contract.methods.balanceOf(walletAddress).call();
 
     return balance
 }
@@ -29,22 +71,27 @@ exports.lambdaHandler = async (event, context) => {
         //for each user
         for(const element of result.Items){
         // result.Items.forEach(async function (element, index, array) {
-            
+
             //if user has a wallet
             if(element.wallets && element.wallets.S != ""){
                 const walletAddress = element.wallets.S
 
                 //get eth balance
-                const balance = await getBalance(web3,walletAddress)
+                const ethBalance = await getEthBalance(web3,walletAddress)
+                const kishuBalance = await getKishuBalance(web3,walletAddress)
+                const sanshuBalance = await getSanshuBalance(web3,walletAddress)
+
 
                 const params = {
                     TableName: TABLE_NAME,
                     Key: {
                         "username": element.username
                     },
-                    UpdateExpression: "set balance_eth = :x",
+                    UpdateExpression: "set balance_eth = :x, balance_kishu = :kishu, balance_sanshu = :sanshu",
                     ExpressionAttributeValues: {
-                        ":x": {'S':balance}
+                        ":x": {'S':ethBalance},
+                        ":kishu": {'S':kishuBalance},
+                        ":sanshu": {'S':sanshuBalance}
                     }
                 }
 

b370436b027f92b2810304ad239960833c0ddef9
Brenden Judson <bjudson@trek10.com>
2022-07-12 14:19:59 -0400

    update be-cicd

--- infrastructure/be-cicd/deploy.sh
+++ infrastructure/be-cicd/deploy.sh
@@ -1,17 +1,17 @@
-aws cloudformation deploy \
-    --stack-name klubby-backend-cicd-prod \
-    --template template.yaml \
-    --region us-east-1 \
-    --capabilities CAPABILITY_IAM \
-    --parameter-override \
-        GitHubOAuthToken=$1 \
-        Stage=prod \
-        GitHubBranch=main
-
 # aws cloudformation deploy \
-#     --stack-name klubby-backend-cicd-dev \
+#     --stack-name klubby-backend-cicd-prod \
 #     --template template.yaml \
 #     --region us-east-1 \
 #     --capabilities CAPABILITY_IAM \
 #     --parameter-override \
-#         GitHubOAuthToken=$1
+#         GitHubOAuthToken=$1 \
+#         Stage=prod \
+#         GitHubBranch=main
+
+aws cloudformation deploy \
+    --stack-name klubby-backend-cicd-dev \
+    --template template.yaml \
+    --region us-east-1 \
+    --capabilities CAPABILITY_IAM \
+    --parameter-override \
+        GitHubOAuthToken=$1

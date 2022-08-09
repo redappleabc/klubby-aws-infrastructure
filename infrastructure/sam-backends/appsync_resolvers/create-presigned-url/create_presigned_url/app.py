eed5cfb36032d3ca2dc31849579b40ebcf8511c3
Brenden Judson <bjudson@trek10.com>
2022-08-09 19:53:56 -0400

    get object key from event

--- infrastructure/sam-backends/appsync_resolvers/create-presigned-url/create_presigned_url/app.py
+++ infrastructure/sam-backends/appsync_resolvers/create-presigned-url/create_presigned_url/app.py
@@ -11,14 +11,16 @@ Stage = os.getenv('STAGE')
 def lambda_handler(event, context):
     try:
         print(f'event {event}')
+        #Format object_key from event info
+        klubname = event['arguments']['key']
+        object_key=f"klub-avatars/{klubname}"
+
 
         #get klub table name from ssm
         response = ssm_client.get_parameter(Name=f'klub-avatar-bucket-name-{Stage}')
         bucket_name=response['Parameter']['Value']
 
-        key="/brenden-test/brenden-test"
-
-        params = {"Bucket": bucket_name,"Key": key,"ContentType": 'text/plain;charset=UTF-8'}
+        params = {"Bucket": bucket_name,"Key": object_key,"ContentType": 'text/plain;charset=UTF-8'}
 
         # result = s3_client.generate_presigned_post(Bucket=bucket_name,Key=key)
         result = s3_client.generate_presigned_url(ClientMethod="put_object",Params=params)

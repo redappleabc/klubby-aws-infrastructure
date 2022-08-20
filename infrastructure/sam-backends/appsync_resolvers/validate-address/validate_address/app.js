3822c3a299c52898b1e2fa30f939455f318e84bd
Brenden Judson <bjudson@trek10.com>
2022-08-20 07:50:41 -0400

    add validate address sam function

---
+++ infrastructure/sam-backends/appsync_resolvers/validate-address/validate_address/app.js
@@ -0,0 +1,292 @@
+let response;
+
+const AWS = require('aws-sdk');
+const Web3 = require('web3')
+
+const RPC_URL="http://18.206.231.219:8545"
+
+// The minimum ABI to get ERC20 Token balance
+const minABI = [
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_spender",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "approve",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_from",
+                "type": "address"
+            },
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "name": "balance",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            },
+            {
+                "name": "_spender",
+                "type": "address"
+            }
+        ],
+        "name": "allowance",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "fallback"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "owner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Approval",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "from",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Transfer",
+        "type": "event"
+    }
+];
+  
+
+async function get_ssm_param(ssm_param_name){
+    const ssm = new AWS.SSM();
+    const response = await ssm.getParameter({
+        Name: ssm_param_name
+    }).promise();
+
+    console.log(response.Parameter.Value)
+    console.log(typeof response.Parameter.Value)
+
+    return response.Parameter.Value
+}
+
+exports.lambdaHandler = async (event, context) => {
+    try {
+        //conect to GETH node
+        const web3 = new Web3(RPC_URL)
+
+        let address = event.arguments.address
+        let assetType = event.arguments.assetType
+
+        try{
+            const contract = new web3.eth.Contract(minABI,address);
+            console.log(contract)
+
+            const totalSupply = await contract.methods.totalSupply().call();
+            const name = await contract.methods.name().call();
+            const symbol = await contract.methods.symbol().call();
+
+            console.log(totalSupply,name,symbol)
+
+
+            return {
+                'statusCode': 200,
+                'body': JSON.stringify({
+                    validAddress: true,
+                    contractInfo: {
+                        name,
+                        symbol,
+                        totalSupply,
+                    }
+
+                })
+            }
+        }
+
+        catch (err) {
+            return {
+                'statusCode': 200,
+                'body': JSON.stringify({
+                    err: err,
+                    validAddress: false
+                })
+            }
+        }
+
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+};

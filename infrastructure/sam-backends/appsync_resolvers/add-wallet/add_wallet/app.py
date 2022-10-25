ed22e7e3e41b80ed1c157feb570bde4f43e5289c
Brenden Judson <bjudson@trek10.com>
2022-10-25 14:16:43 -0400

    return new user item

--- infrastructure/sam-backends/appsync_resolvers/add-wallet/add_wallet/app.py
+++ infrastructure/sam-backends/appsync_resolvers/add-wallet/add_wallet/app.py
@@ -113,7 +113,6 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
+            user_item
         }),
     }

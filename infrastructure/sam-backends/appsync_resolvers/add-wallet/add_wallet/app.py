964e2473c551e39ff095045fd8535c093e485c2b
Brenden Judson <bjudson@trek10.com>
2022-10-26 14:32:00 -0400

    fix missing variable error

--- infrastructure/sam-backends/appsync_resolvers/add-wallet/add_wallet/app.py
+++ infrastructure/sam-backends/appsync_resolvers/add-wallet/add_wallet/app.py
@@ -100,10 +100,10 @@ def lambda_handler(event, context):
 
         balance = get_asset_balance(clean_data,wallet_address)
 
-        asset_address = w3.toChecksumAddress(asset['address'])
+        # asset_address = w3.toChecksumAddress(asset['address']['S'])
 
         if balance > 0:
-            asset_list.append({'M': {'balance':{'N':str(balance)},'symbol': {'S':asset['symbol']['S']},'name': {'S':asset['name']['S']}, 'contractType': {'S': asset['contractType']['S']}, 'address': {'S':asset_address}}})
+            asset_list.append({'M': {'balance':{'N':str(balance)},'symbol': {'S':asset['symbol']['S']},'name': {'S':asset['name']['S']}, 'contractType': {'S': asset['contractType']['S']}, 'address': asset['address']}})
     
     #update assets field
     user_item['assets'] = {'L': asset_list}

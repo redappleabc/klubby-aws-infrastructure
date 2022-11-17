14d8bbac07fa362d551811a040e03539dd9c0c90
Brenden Judson <bjudson@trek10.com>
2022-11-16 19:06:45 -0500

    add attached-file-presigned-url function

---
+++ infrastructure/sam-backends/appsync_resolvers/create-attached-file-presigned-url/create_attached_file_presigned_url/app.py
@@ -0,0 +1,46 @@
+import json
+import boto3
+import os
+
+s3_client = boto3.client('s3')
+ssm_client = boto3.client('ssm')
+
+#get stage from env var
+# Stage = os.getenv('STAGE')
+Stage = 'dev'
+
+def lambda_handler(event, context):
+    try:
+        print(f'event {event}')
+
+        #Format object_key from event info
+        conversationId = event['arguments']['conversationId']
+
+        object_key=f"attached-files-avatars/{conversationId}/timestamp"
+
+
+        #get klub table name from ssm
+        response = ssm_client.get_parameter(Name=f'attached-files-bucket-name-{Stage}')
+        bucket_name=response['Parameter']['Value']
+
+        params = {"Bucket": bucket_name,"Key": object_key,"ContentType": 'text/plain;charset=UTF-8',"ACL": "public-read"}
+
+        # result = s3_client.generate_presigned_post(Bucket=bucket_name,Key=key)
+        result = s3_client.generate_presigned_url(ClientMethod="put_object",Params=params)
+
+        print(result)
+
+    except Exception as e:
+        return json.dumps({
+            "statusCode": 500,
+            "body": {
+                "error": f'{e}'
+            }
+        })
+
+    return json.dumps({
+        "statusCode": 200,
+        "body": {
+            "url_info": result
+        }
+    })

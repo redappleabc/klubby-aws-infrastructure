178be176d6ee6cb355604ec01720261ceca369c7
Brenden Judson <bjudson@trek10.com>
2022-11-20 17:35:23 -0500

    add try catch

--- infrastructure/sam-backends/appsync_resolvers/delete-klub/delete_klub/app.py
+++ infrastructure/sam-backends/appsync_resolvers/delete-klub/delete_klub/app.py
@@ -10,85 +10,96 @@ ssm_client = boto3.client('ssm')
 Stage = os.getenv('STAGE')
 
 def lambda_handler(event, context):
-    print(f'event {event}')
- 
-    #get params from event
-    klubname = event['arguments']['klubname']
+    try:
+        print(f'event {event}')
 
-    #get klub table name from ssm
-    response = ssm_client.get_parameter(Name=f'klub-table-name-{Stage}')
-    klub_table_name=response['Parameter']['Value']
+        #get params from event
+        klubname = event['arguments']['klubname']
 
-    #get conversation table name from ssm
-    response = ssm_client.get_parameter(Name=f'conversation-table-name-{Stage}')
-    conversation_table_name=response['Parameter']['Value']
+        #get klub table name from ssm
+        response = ssm_client.get_parameter(Name=f'klub-table-name-{Stage}')
+        klub_table_name=response['Parameter']['Value']
 
-    #get message table name from ssm
-    response = ssm_client.get_parameter(Name=f'message-table-name-{Stage}')
-    message_table_name=response['Parameter']['Value']
+        #get conversation table name from ssm
+        response = ssm_client.get_parameter(Name=f'conversation-table-name-{Stage}')
+        conversation_table_name=response['Parameter']['Value']
 
-    #get UserKlubBridge table name from ssm
-    response = ssm_client.get_parameter(Name=f'userklubbridge-table-name-{Stage}')
-    userklubbridge_table_name=response['Parameter']['Value']
+        #get message table name from ssm
+        response = ssm_client.get_parameter(Name=f'message-table-name-{Stage}')
+        message_table_name=response['Parameter']['Value']
 
-    #get klub info
-    result = dynamodb.get_item(TableName=klub_table_name, Key={'klubname':{'S':klubname}})
-    klub_info = result['Item']
+        #get UserKlubBridge table name from ssm
+        response = ssm_client.get_parameter(Name=f'userklubbridge-table-name-{Stage}')
+        userklubbridge_table_name=response['Parameter']['Value']
 
-    avatar_url = klub_info['avatar_url']['S']
-    mainGroupConversationId = klub_info['mainGroupConversationId']['S']
-    whaleGroupConversationId = klub_info['whaleGroupConversationId']['S']
-    announcementConversationId = klub_info['announcementConversationId']['S']
+        #get klub info
+        result = dynamodb.get_item(TableName=klub_table_name, Key={'klubname':{'S':klubname}})
+        klub_info = result['Item']
+
+        avatar_url = klub_info['avatar_url']['S']
+        mainGroupConversationId = klub_info['mainGroupConversationId']['S']
+        whaleGroupConversationId = klub_info['whaleGroupConversationId']['S']
+        announcementConversationId = klub_info['announcementConversationId']['S']
 
 
-    # ---- delete avatar_url ----
-    #parse url into bucket name and object key
-    bucket_name = avatar_url.split("/")[2][:-17]
-    object_name = avatar_url.split("/")[-1]
+        # ---- delete avatar_url ----
+        #parse url into bucket name and object key
+        bucket_name = avatar_url.split("/")[2][:-17]
+        object_name = avatar_url.split("/")[-1]
 
-    response = s3.delete_object(
-        Bucket=bucket_name,
-        Key=f'klub-avatars/{object_name}'
-    )
+        response = s3.delete_object(
+            Bucket=bucket_name,
+            Key=f'klub-avatars/{object_name}'
+        )
 
-    # ---- delete mainGroupConversation ----
-    result = dynamodb.delete_item(TableName=conversation_table_name, Key={'id':{'S':mainGroupConversationId}})
+        # ---- delete mainGroupConversation ----
+        result = dynamodb.delete_item(TableName=conversation_table_name, Key={'id':{'S':mainGroupConversationId}})
 
-    #delete messages associated with conversation
-    result = dynamodb.scan(TableName=message_table_name,FilterExpression=f'conversationId = :mainGroupConversationId',ExpressionAttributeValues={':mainGroupConversationId': {'S': mainGroupConversationId}})
+        #delete messages associated with conversation
+        result = dynamodb.scan(TableName=message_table_name,FilterExpression=f'conversationId = :mainGroupConversationId',ExpressionAttributeValues={':mainGroupConversationId': {'S': mainGroupConversationId}})
 
-    for message in result['Items']:
-        result = dynamodb.delete_item(TableName=message_table_name, Key={'id': message['id'],'conversationId': message['conversationId'],})
+        for message in result['Items']:
+            result = dynamodb.delete_item(TableName=message_table_name, Key={'id': message['id'],'conversationId': message['conversationId'],})
 
-    # ---- delete whaleGroupConversation ----
-    result = dynamodb.delete_item(TableName=conversation_table_name, Key={'id':{'S':whaleGroupConversationId}})
+        # ---- delete whaleGroupConversation ----
+        result = dynamodb.delete_item(TableName=conversation_table_name, Key={'id':{'S':whaleGroupConversationId}})
 
-    #delete messages associated with conversation
-    result = dynamodb.scan(TableName=message_table_name,FilterExpression=f'conversationId = :whaleGroupConversationId',ExpressionAttributeValues={':whaleGroupConversationId': {'S': whaleGroupConversationId}})
+        #delete messages associated with conversation
+        result = dynamodb.scan(TableName=message_table_name,FilterExpression=f'conversationId = :whaleGroupConversationId',ExpressionAttributeValues={':whaleGroupConversationId': {'S': whaleGroupConversationId}})
 
-    for message in result['Items']:
-        result = dynamodb.delete_item(TableName=message_table_name, Key={'id': message['id'],'conversationId': message['conversationId'],})
+        for message in result['Items']:
+            result = dynamodb.delete_item(TableName=message_table_name, Key={'id': message['id'],'conversationId': message['conversationId'],})
 
-    # ---- delete announcementConversation ----
-    result = dynamodb.delete_item(TableName=conversation_table_name, Key={'id':{'S':announcementConversationId}})
+        # ---- delete announcementConversation ----
+        result = dynamodb.delete_item(TableName=conversation_table_name, Key={'id':{'S':announcementConversationId}})
 
-    #delete messages associated with conversation
-    result = dynamodb.scan(TableName=message_table_name,FilterExpression=f'conversationId = :announcementConversationId',ExpressionAttributeValues={':announcementConversationId': {'S': announcementConversationId}})
+        #delete messages associated with conversation
+        result = dynamodb.scan(TableName=message_table_name,FilterExpression=f'conversationId = :announcementConversationId',ExpressionAttributeValues={':announcementConversationId': {'S': announcementConversationId}})
 
-    for message in result['Items']:
-        result = dynamodb.delete_item(TableName=message_table_name, Key={'id': message['id'],'conversationId': message['conversationId'],})
+        for message in result['Items']:
+            result = dynamodb.delete_item(TableName=message_table_name, Key={'id': message['id'],'conversationId': message['conversationId'],})
 
-    # ---- delete User Klub associations in UserKlubBridge Table ----
-    #delete user klub associations that have this klub
-    result = dynamodb.scan(TableName=userklubbridge_table_name,FilterExpression=f'klubname = :klubname',ExpressionAttributeValues={':klubname': {'S': klubname}})
-    for klub_member in result['Items']:
-        result = dynamodb.delete_item(TableName=userklubbridge_table_name, Key={'klubname': klub_member['klubname'],'username': klub_member['username'],})
-    print(result)
+        # ---- delete User Klub associations in UserKlubBridge Table ----
+        #delete user klub associations that have this klub
+        result = dynamodb.scan(TableName=userklubbridge_table_name,FilterExpression=f'klubname = :klubname',ExpressionAttributeValues={':klubname': {'S': klubname}})
+        for klub_member in result['Items']:
+            result = dynamodb.delete_item(TableName=userklubbridge_table_name, Key={'klubname': klub_member['klubname'],'username': klub_member['username'],})
+        print(result)
 
-    # --- delete klub ---
-    result = dynamodb.delete_item(TableName=klub_table_name, Key={'klubname':{'S':klubname}})
+        # --- delete klub ---
+        result = dynamodb.delete_item(TableName=klub_table_name, Key={'klubname':{'S':klubname}})
 
-    return {
-            "statusCode": 200,
-            "body": "yo"
+    except Exception as e:
+        return json.dumps({
+            "statusCode": 500,
+            "body": {
+                "error": f'{e}'
+            }
+        })
+
+    return json.dumps({
+        "statusCode": 200,
+        "body": {
+            "url_info": result
         }
+    })

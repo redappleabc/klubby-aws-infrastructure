afe0e1cf0bc41761cac1f288578ed9c429e02cae
Brenden Judson <bjudson@trek10.com>
2022-11-29 06:52:39 -0500

    add ssm to aws lib

--- src/lib/aws/SSM.py
+++ src/lib/aws/SSM.py
@@ -5,7 +5,7 @@ class SSM():
     def __init__(self):
         self.client = boto3.client('ssm', region_name='us-east-1')
 
-    def getParameterValue(param_name):
+    def getParameterValue(self,param_name):
         response = ssm_client.get_parameter(Name=param_name)
         param_val = response['Parameter']['Value']
         return param_val

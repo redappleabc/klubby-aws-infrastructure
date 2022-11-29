3aff125d661b3cfc3b4613b917f9ed272683dfbf
Brenden Judson <bjudson@trek10.com>
2022-11-28 19:02:54 -0500

    add region name to s3

--- src/lib/aws/S3.py
+++ src/lib/aws/S3.py
@@ -3,5 +3,5 @@ import boto3
 
 class S3():
     def __init__(self, bucket_name):
-        self.client = boto3.client('s3')
+        self.client = boto3.client('s3', region_name='us-east-1')
         self.bucket_name = bucket_name

64f0b5e7792227cbb25b0aa09b315ce2fe951bae
Brenden Judson <bjudson@trek10.com>
2022-11-29 07:23:57 -0500

    add web3 to lib

---
+++ src/lib/Web3.py
@@ -0,0 +1,24 @@
+from web3 import Web3
+
+class Web3():
+    def __init__(self):
+        # self.client = Web3(Web3.HTTPProvider(RPC_URL))
+        self.client = Web3(Web3.HTTPProvider(INFURA_URL))
+
+    def getParameterValue(self,param_name):
+        response = self.client.get_parameter(Name=param_name)
+        param_val = response['Parameter']['Value']
+        return param_val
+
+
+def decodeIpfsUrl(ipfs_url):
+    #check if string starts with 'https://'
+    if ipfs_url.startswith('https://'):
+        return ipfs_url
+
+    # prefix = "https://mainnet.infura-ipfs.io/ipfs/"
+    prefix = "https://cloudflare-ipfs.com/ipfs/"
+
+    hash = ipfs_url.replace('ipfs://', '')
+
+    return prefix + hash
